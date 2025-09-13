1165defc94cdb5543fd782ddf4d0f61be5baedda
Kate Belova <thebrightestcry@gmail.com>
2025-09-13 11:21:27 +0300

    еще раз переделала задание 3

--- homework/kate_belova/homework7/task3.py
+++ homework/kate_belova/homework7/task3.py
@@ -1,9 +1,16 @@
-def extract_number(lines: list) -> int | None:
-    for line in lines:
+def extract_numbers(str_lines: list) -> list[int]:
+    numbers_lst = []
+    for line in str_lines:
         num = int(line.split(':')[1].strip())
-        return num
-    return None
+        numbers_lst.append(num)
+    return numbers_lst
 
 
-number = extract_number(input('Введите строки: ').split()) + 10
-print(number)
+lines = [
+    'результат операции: 42',
+    'результат операции: 54',
+    'результат работы программы: 209',
+    'результат: 2',
+]
+numbers = [num + 10 for num in extract_numbers(lines)]
+print(*numbers)
